92c5912ef6bb4141b3e01fac323b9a6cbbec389b
Denis Alonso <denisalonso1999@gmail.com>
2025-03-20 19:58:03 -0300

    DataTest

---
+++ insee/src/test/java/DataTest.java
@@ -0,0 +1,10 @@
+import br.edu.insper.desagil.aps5.insee.Data;
+import org.junit.jupiter.api.Test;
+
+public class DataTest {
+    @Test
+    void constroi () {
+        Data data = new Data();
+    }
+
+}
