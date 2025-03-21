2169e9fec0c5359bbf95aa8cf4a73b55aacbf3a5
Denis Alonso <denisalonso1999@gmail.com>
2025-03-20 22:11:47 -0300

    remocao de biblios nao utilizadas

--- insee/src/test/java/br/edu/insper/desagil/aps5/insee/AgendaTest.java
+++ insee/src/test/java/br/edu/insper/desagil/aps5/insee/AgendaTest.java
@@ -2,9 +2,6 @@ package br.edu.insper.desagil.aps5.insee;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AgendaTest {
